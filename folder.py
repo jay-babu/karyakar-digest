000c2d86d730646ab42382901d06a0add3209ea9
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2021-09-01 19:27:19 -0400

    Update folder to create tuesday sub folder

--- folder.py
+++ folder.py
@@ -1,7 +1,44 @@
 import os
+from datetime import date, timedelta
+
+
+def allsundays(year):
+    d = date(year, 1, 1)  # January 1st
+    d += timedelta(days=8 - d.weekday())  # First Sunday
+    while d.year == year:
+        yield d
+        d += timedelta(days=7)
+
+
+folder_names = [
+    '01. January',
+    '02. February',
+    '03. March',
+    '04. April',
+    '05. May',
+    '06. June',
+    '07. July',
+    '08. August',
+    '09. September',
+    '10. October',
+    '11. November',
+    '12. December',
+]
+
 
 def create_folders():
-    for folder in ['01. January', '02. February', '03. March', '04. April', '05. May', '06. June', '07. July', '08. August', '09. September','10. October', '11. November', '12. December']:
+    for folder in folder_names:
         os.mkdir(folder)
 
-create_folders()
+
+if __name__ == '__main__':
+    create_folders()
+
+    months = {count: month for (count, month) in enumerate(folder_names, start=1)}
+
+    for d in allsundays(2021):
+        print(d.strftime('%m-%d-%Y'))
+        sub = d.strftime('%m-%d-%Y')
+        folder_name = months.get(d.month)
+        os.mkdir(os.path.join(folder_name, sub))
+        print(d.month, folder_name)
